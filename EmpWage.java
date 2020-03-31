1deb58ed9436953e849ea50fa015e5bb1c6ec962
vivek kumar <vivekhaarit@gmail.com>
2020-03-31 19:37:16 +0530

    [vivek]Added:part time worker

--- EmpWage.java
+++ EmpWage.java
@@ -2,14 +2,17 @@ public class EmpWage{
 	public static void main(String[] args){
 		//constants
 		int IS_FULL_TIME=1;
+		int IS_PART_TIME=2;
 		int EMP_RATE_PER_HOUR=	20;
 	
 		//variables
 		int empHrs=0;
 		int empWage=0;
-		double empCheck=Math.floor(Math.random()*10)%2;
+		double empCheck=Math.floor(Math.random()*10)%3;
 		if(empCheck ==IS_FULL_TIME)
 			empHrs=8;
+		else if(empCheck == IS_PART_TIME)
+			empHrs=4;
 		else
 			empHrs=0;		
 		empWage=empHrs*EMP_RATE_PER_HOUR;

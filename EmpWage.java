b41c085d4bd79165b93b72d01601fa6628538f07
vivek kumar <vivekhaarit@gmail.com>
2020-03-31 19:32:22 +0530

    [vivek]Added:Daily wage calculated

--- EmpWage.java
+++ EmpWage.java
@@ -1,10 +1,18 @@
 public class EmpWage{
 	public static void main(String[] args){
+		//constants
 		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=	20;
+	
+		//variables
+		int empHrs=0;
+		int empWage=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck ==IS_FULL_TIME)
-			System.out.println("Employee is present");
+			empHrs=8;
 		else
-			System.out.println("Employee is absent");	
+			empHrs=0;		
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("emp wage: "+empWage);	
 	}
 }

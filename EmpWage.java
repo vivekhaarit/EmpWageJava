5f2175f6ef2722681618511782cf43a60654a702
vivek kumar <vivekhaarit@gmail.com>
2020-03-31 19:45:01 +0530

    [vivek]Added:solved using switch stmt

--- EmpWage.java
+++ EmpWage.java
@@ -1,18 +1,25 @@
 public class EmpWage{
+	//constants
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+	
+	
 	public static void main(String[] args){
-		int IS_FULL_TIME=1;
-		int IS_PART_TIME=2;
-		int EMP_RATE_PER_HOUR=	20;
 	
 		int empHrs=0;
 		int empWage=0;
-		double empCheck=Math.floor(Math.random()*10)%3;
-		if(empCheck ==IS_FULL_TIME)
-			empHrs=8;
-		else if(empCheck == IS_PART_TIME)
-			empHrs=4;
-		else
-			empHrs=0;		
+		int empCheck=(int)Math.floor(Math.random()*10)%3;
+		switch (empCheck){
+			case IS_FULL_TIME:
+				empHrs=8;
+				break;
+			case IS_PART_TIME:
+				empHrs=4;
+				break;
+			default:
+				empHrs=0;
+		}		
 		empWage=empHrs*EMP_RATE_PER_HOUR;
 		System.out.println("emp wage: "+empWage);	
 	}
